3c6a613084bca736f9ae423fe1d9aa445655fa23
Aaron Smith <magik_smith@outlook.com>
2020-04-04 17:35:21 -0400

    added spawner objective

--- Assets/Marcus's Assets/Scripts/ObjectiveKillSpawners.cs
+++ Assets/Marcus's Assets/Scripts/ObjectiveKillSpawners.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class ObjectiveKillSpawners : MonoBehaviour
 {
     //[HideInInspector]
-    public int remainingSpawners = 1;
+    public int remainingSpawners = 0;
 
     Objective m_Objective;
+    private bool objectiveCompleted = false;
+    private int maxSpawners;
+    private int notificationEnemiesRemainingThreshold;
+
     // Start is called before the first frame update
     void Start()
     {
         m_Objective = GetComponent<Objective>();
         DebugUtility.HandleErrorIfNullGetComponent<Objective, ObjectiveKillSpawners>(m_Objective, this, gameObject);
+
+        maxSpawners = remainingSpawners;
+
+        if (string.IsNullOrEmpty(m_Objective.description))
+            m_Objective.description = GetUpdatedCounterAmount();
     }
 
     // Update is called once per frame
@@ -22,10 +31,22 @@ public class ObjectiveKillSpawners : MonoBehaviour
     }
     void CompleteObjective()
     {
-        if (remainingSpawners <= 0)
+        if (remainingSpawners <= 0 && objectiveCompleted == false)
         {
-            m_Objective.CompleteObjective(string.Empty, "1", "Objective complete : " + m_Objective.title);
+            m_Objective.CompleteObjective("Killed all enemy spawners!", "1", "Objective complete : " + m_Objective.title);
+            objectiveCompleted = true;
             //objective.CompleteObjective("Killed all enemy spawners!", "1", "Objective complete : " + objective.title);
         }
+    }
+    string GetUpdatedCounterAmount()
+    {
+        return remainingSpawners + " / " + maxSpawners;
+    }
+    public void UpdateDescription()
+    {
+        string notificationText = notificationEnemiesRemainingThreshold >= remainingSpawners ? remainingSpawners + " spawners to kill left" : string.Empty;
+
+        m_Objective.UpdateObjective(string.Empty, GetUpdatedCounterAmount(), notificationText);
+        //m_Objective.description = GetUpdatedCounterAmount();
     }
 }
--- Assets/Marcus's Assets/Scripts/Spawner.cs
+++ Assets/Marcus's Assets/Scripts/Spawner.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Spawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
     public Transform spawnPos;
 
+    private ObjectiveKillSpawners objective;
+    private Health health;
+
     public float spawningRate = 2;
+
+    public GameObject deathVFX;
+    [Tooltip("The point at which the death VFX is spawned")]
+    public Transform deathVFXSpawnPoint;
+
+    [Header("Loot")]
+    [Tooltip("The object this enemy can drop when dying")]
+    public GameObject lootPrefab;
+    [Tooltip("The chance the object has to drop")]
+    [Range(0, 1)]
+    public float deathDuration = 0f;
+
+    public float dropRate = 1f;
+
     [HideInInspector]
     public int totalObjectsSpawned;
     private float lastSpawnTime = Mathf.NegativeInfinity;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        objective = FindObjectOfType<ObjectiveKillSpawners>();
+        objective.remainingSpawners++;
+
+        health = GetComponent<Health>();
+        health.onDie += OnDie;
     }
 
     // Update is called once per frame
@@ -30,5 +52,30 @@ public class Spawner : MonoBehaviour
             totalObjectsSpawned++;
             lastSpawnTime = Time.time;
         }
+    }
+    void OnDie()
+    {
+        // spawn a particle system when dying
+        var vfx = Instantiate(deathVFX, deathVFXSpawnPoint.position, Quaternion.identity);
+        Destroy(vfx, 5f);
+
+        // loot an object
+        if (TryDropItem())
+        {
+            Instantiate(lootPrefab, transform.position, Quaternion.identity);
+        }
+        objective.remainingSpawners--;
+        objective.UpdateDescription();
+        // this will call the OnDestroy function
+        Destroy(gameObject, deathDuration);
+    }
+    public bool TryDropItem()
+    {
+        if (dropRate == 0 || lootPrefab == null)
+            return false;
+        else if (dropRate == 1)
+            return true;
+        else
+            return (Random.value <= dropRate);
     }
 }
