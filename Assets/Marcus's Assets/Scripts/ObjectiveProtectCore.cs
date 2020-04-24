a9af9b151110edd615b82c1ffdb11d154a2290f8
Aaron Smith <magik_smith@outlook.com>
2020-04-23 21:25:47 -0400

    added an inner wall

--- Assets/Marcus's Assets/Scripts/ObjectiveProtectCore.cs
+++ Assets/Marcus's Assets/Scripts/ObjectiveProtectCore.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Objective))]
 public class ObjectiveProtectCore : MonoBehaviour
 {
+    float timeInSeconds = 0;
+    float timeInMin = 0;
+    float timeInHr = 0;
+
+    GameFlowManager manager;
     Objective objective;
     // Start is called before the first frame update
     void Start()
     {
         objective = GetComponent<Objective>();
+        manager = FindObjectOfType<GameFlowManager>();
     }
 
     // Update is called once per frame
@@ -19,8 +25,22 @@ public class ObjectiveProtectCore : MonoBehaviour
     }
     public void UpdateDescription()
     {
-        string notificationText = Time.time.ToString();
+        if(manager.gameIsEnding  == false)
+        {
+            timeInSeconds += Time.deltaTime;
+            if (timeInSeconds >= 60)
+            {
+                timeInMin++;
+                timeInSeconds = 0;
+            }
+            if (timeInMin >= 60)
+            {
+                timeInHr++;
+                timeInMin = 0;
+            }
+            string notificationText = "Core lifetime : " + timeInHr + ":" + timeInMin + ":" + Mathf.Floor(timeInSeconds);
 
-        objective.UpdateObjective("Core lifetime (In seconds) : "+notificationText, "", "");
+            objective.UpdateObjective(notificationText, "", "");
+        }
     }
 }
